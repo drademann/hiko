3ad269d48ff73ea76bdbb72a978a4e81ce060e8d
Dirk Rademann <dirk.rademann@icloud.com>
2025-09-04 21:19:45 +0200

    moving projects to fit the desired folder structure

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
   displayName: 'backend',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/backend',
+  coverageDirectory: '../../coverage/apps/backend',
   setupFilesAfterEnv: ['<rootDir>/src/test.setup.ts'],
 };
